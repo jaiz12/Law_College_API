4944ea40948f407bb3f3825d77e3f76427ccc527
jaideep.roy <jaideep.roy@sibintechnologies.com>
2026-08-18 16:38:01 +0530

    academic calendar bug fix

--- DTO/Models/Academics/AcademicCalendarDTO.cs
+++ DTO/Models/Academics/AcademicCalendarDTO.cs
@@ -13,7 +13,7 @@ namespace DTO.Models.Academics
 
         public string? Title { get; set; }
 
-        public string? Content { get; set; }
+        public string? Content { get; set; } 
 
         public string? FilePath { get; set; }
         public IFormFile? File { get; set; }
